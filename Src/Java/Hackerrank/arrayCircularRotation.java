2421d93e79087334e6c1c728bb0a6d17e1f54c7a
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2017-01-14 03:09:15 -0800

    Update arrayCircularRotation.java

--- Src/Java/Hackerrank/arrayCircularRotation.java
+++ Src/Java/Hackerrank/arrayCircularRotation.java
@@ -1,19 +1,7 @@
 /*
-John Watson performs an operation called a right circular rotation on an array of integers, . After performing one right circular rotation operation, the array is transformed from to .
 
-Watson performs this operation times. To test Sherlock's ability to identify the current element at a particular position in the rotated array, Watson asks queries, where each query consists of a single integer, , for which you must print the element at index in the rotated array (i.e., the value of ).
+Question link : https://www.hackerrank.com/challenges/circular-array-rotation
 
-Input Format
-
-The first line contains space-separated integers, , , and , respectively.
-The second line contains space-separated integers, where each integer describes array element (where ).
-Each of the subsequent lines contains a single integer denoting .
-
-Constraints
-
-Output Format
-
-For each query, print the value of the element at index of the rotated array on a new line.
 */
 
 package src.java.hackerrank;
