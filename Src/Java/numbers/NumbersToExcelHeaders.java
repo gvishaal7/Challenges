6e7c1cd92455f20be92969dfcc7509bb2ef237d6
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2017-01-13 20:26:16 -0800

    Update NumbersToExcelHeaders.java

--- Src/Java/numbers/NumbersToExcelHeaders.java
+++ Src/Java/numbers/NumbersToExcelHeaders.java
@@ -16,81 +16,72 @@ public class NumbersToExcelHeaders {
      * @param args the command line arguments
      */
     
-    public static String getAlpha(int number)
-    {
+    public static String getAlpha(int number) {
         String alpha = "";
-        number += 64;
-        alpha = Character.toString((char)number);
+        try {
+            number += 64;
+            alpha = Character.toString((char)number);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
         return alpha;
     }
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        BufferedReader br = null;
-        try
-        {
-            br = new BufferedReader(new InputStreamReader(System.in));
-            int input = 0;
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner((System.in));
             System.out.println("Enter the number : ");
-            String in = br.readLine();
-            try 
-            {
-                input = Integer.parseInt(in);
+            int input = scanner.nextInt();
+            try {
                 do {
                     if(input<0) {
                         System.out.println("Enter a positive number");
+                        break;
                     }
                     else {
                         int num =0;
                         int deno =0;
                         String output = "";         
-                            if(input <=26)
-                            {
+                        if(input <=26) {
                                 output = getAlpha(input);
+                        }
+                        else {
+                            do {
+                                num = input/26;
+                                deno = input % 26;
+                                if(deno == 0) { num = num - 1;}
+                                output += getAlpha(num);
+                                num = num /26;
+                            } while(num > 0);
+                            if(deno == 0 && input > 26) {
+                                deno = 26;
                             }
-                            else
-                            {
-                                do
-                                {
-                                    num = input/26;
-                                    deno = input % 26;
-                                    if(deno == 0) { num = num - 1;}
-                                    output += getAlpha(num);
-                                    num = num /26;
-                                }while(num > 0);
-                                if(deno == 0 && input > 26)
-                                    deno = 26;
-                                output += getAlpha(deno);
-                            }
-                        System.out.println("OUTPUT :: "+output);
+                            output += getAlpha(deno);
+                        }    
+                    System.out.println("OUTPUT :: "+output);    
                     }
-                }while(input<0);
+                } while(input<0);
             }
-            catch(NumberFormatException ne)
-            {
+            catch(NumberFormatException ne) {
                 System.out.println("Enter an Integer");
-            //    System.out.println(ne.toString());
             }
-            catch(Exception e)
-            {
+            catch(Exception e) {
                 e.printStackTrace();
             }
-            
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             e.printStackTrace();
         }
-        finally
-        {
-            try
-            {
-                if(br!=null)
-                {
-                    br.close();
+        finally {
+            try {
+                if(scanner!=null) {
+                    scanner.close();
                 }
             }
-            catch(Exception e)
-            {
+            catch(Exception e) {
                 e.printStackTrace();
             }
         }
