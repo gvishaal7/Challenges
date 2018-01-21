959e0a55e64eff7c7924eb35f45e00eb177649c8
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-01-20 23:54:50 -0800

    Create odd_even_ll.java

---
+++ Src/leetcode/odd_even_ll.java
@@ -0,0 +1,33 @@
+/*
+question link : https://leetcode.com/problems/odd-even-linked-list/description/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head != null && head.next != null) {
+            ListNode odd = head;
+            ListNode odd1 = odd;
+            ListNode even = head.next;
+            ListNode even1 = even;
+            while(odd.next != null && even.next !=null) {
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = even1;
+            return odd1;
+        }
+        else {
+            return head;
+        }
+    }
+}
