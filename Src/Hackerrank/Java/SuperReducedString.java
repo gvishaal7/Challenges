24037d6d7e652a2e68d179418d9a0a0f3d050dc6
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-01-29 13:13:56 -0800

    Create SuperReducedString.java

---
+++ Src/Hackerrank/Java/SuperReducedString.java
@@ -0,0 +1,46 @@
+/*
+
+question link : https://www.hackerrank.com/challenges/reduced-string/problem
+
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SuperReducedString {
+
+    static String super_reduced_string(String s){
+        // Complete this function
+        if(s == null) {
+            return null;
+        }
+        else {
+            int i=0;
+            while(!s.equals("") && i != s.length()-1) {
+                if(s.charAt(i) == s.charAt(i+1)) {
+                    String temp = s.substring(0,i);
+                    temp += s.substring(i+2,s.length());
+                    s = temp;
+                    i=0;
+                }
+                else {
+                    i++;
+                }
+            }
+            if(s.equals("")) {
+                s = "Empty String";
+            }
+            return s;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String result = super_reduced_string(s);
+        System.out.println(result);
+    }
+}
