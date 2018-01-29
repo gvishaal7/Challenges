16bed97cb420e0b6fe83e83f7f6e6b54180fbee0
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-01-29 12:27:29 -0800

    Update BinaryTreeNodes.sql

--- Src/Hackerrank/SQL/BinaryTreeNodes.sql
+++ Src/Hackerrank/SQL/BinaryTreeNodes.sql
@@ -1,13 +1,13 @@
 /*link:https://www.hackerrank.com/challenges/binary-search-tree-1/problem*/
 
-select distinct(t1.n), 
-  (CASE WHEN 
-   t1.p is null 
+select 	distinct(t1.n), 
+  	(CASE WHEN 
+   	t1.p is null 
    	then 'Root' 
-   WHEN t2.n is null 
+   	WHEN t2.n is null 
    	then 'Leaf' 
-   ELSE 'Inner' 
-   END) 
-  from BST 
-  t1 left join BST t2 
-  on t1.n=t2.p;
+   	ELSE 'Inner' 
+   	END) 
+from BST t1 
+left join BST t2 
+on t1.n=t2.p;
