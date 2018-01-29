18db97dcb9eb7bee2159adcbf772c3fdb174af25
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-01-29 13:14:16 -0800

    Update commentRemoval.java

--- Src/Hackerrank/Java/commentRemoval.java
+++ Src/Hackerrank/Java/commentRemoval.java
@@ -6,7 +6,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class commentRemoval {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
