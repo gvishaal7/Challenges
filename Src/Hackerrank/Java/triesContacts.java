5655fc08bdc37fd525e3672dbdaaa98958dbe125
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-01-29 13:14:41 -0800

    Update triesContacts.java

--- Src/Hackerrank/Java/triesContacts.java
+++ Src/Hackerrank/Java/triesContacts.java
@@ -8,7 +8,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class triesContacts {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
