12fda11b4d3e3e306fdac81dc81d69043bf17017
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-02-05 13:03:56 -0800

    Update arrayShuffle.java

--- Src/Random/Java/arrayShuffle.java
+++ Src/Random/Java/arrayShuffle.java
@@ -1,12 +1,12 @@
 import java.util.Random;
 
 
-public void shuffle(int[] a) {
+public void shuffle(Object[] a) {
     int aLength = a.length-1;
     Random rand = new Random();
     while(aLength > 0) {
         int swapIndex = rand.nextInt(aLength);
-        int temp = a[swapIndex];
+        Object temp = a[swapIndex];
         a[swapIndex] = a[aLength];
         a[aLength] = temp;
         aLength--;
