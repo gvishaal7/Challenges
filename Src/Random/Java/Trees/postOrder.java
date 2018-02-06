ddd1c55f1bcb392fb54f3d1964bfe81ef3409fa1
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-02-06 12:32:19 -0800

    Update postOrder.java

--- Src/Random/Java/Trees/postOrder.java
+++ Src/Random/Java/Trees/postOrder.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    left - right - root
+*/
 package trees;
 
-/**
- *
- * @author vishaalgopalan
- */
 public class postOrder {
     
     public static void main(String[] args) throws Exception {
