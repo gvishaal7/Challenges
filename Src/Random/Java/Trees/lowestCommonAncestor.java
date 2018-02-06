830dd8350acff11bec5bd95e2a52dfd50ca9c20a
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-02-06 12:42:54 -0800

    Update lowestCommonAncestor.java

--- Src/Random/Java/Trees/lowestCommonAncestor.java
+++ Src/Random/Java/Trees/lowestCommonAncestor.java
@@ -1,18 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trees;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
 
-/**
- *
- * @author vishaalgopalan
- */
 public class lowestCommonAncestor {
     
     public static void main(String[] args) throws Exception {
@@ -23,9 +13,13 @@ public class lowestCommonAncestor {
     public void process() {
         createTree ct = new createTree();
         Node root = ct.setUpTree();
-        /*printTree(root);
-        System.out.println(); */
-        findLCA(root,6,7);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two node values");
+        System.out.print("node 1 : ");
+        int val1 = in.nextInt();
+        System.out.print("node 2 : ");
+        int val2 = in.nextInt();
+        findLCA(root,val1,val2);
     }
     
     public void findLCA(Node root, int val1, int val2) {
@@ -33,35 +27,31 @@ public class lowestCommonAncestor {
             return;
         }
         else if(root.getNodeVal() == val1) {
-            System.out.println(val1);
+            System.out.println("The common ancester is : "+val1);
         }
         else if(root.getNodeVal() == val2) {
-            System.out.println(val2);
+            System.out.println("The common ancester is : "+val2);
         }
         else {
-            Stack<Integer> path1 = new Stack<>();
-            Stack<Integer> path2 = new Stack<>();
             Node temp = root;
-            path1 = findPath(temp,val1);
+            Stack<Integer> path1 = findPath(temp,val1);
             temp = root;
-            path2 = findPath(temp,val2);
-            boolean flag = false;
+            Stack<Integer> path2 = findPath(temp,val2);
             int common = 0;
-            while(!path1.isEmpty() && !path2.isEmpty()) {
-                int tree1 = path1.pop();
-                int tree2 = path2.pop();
-                if(tree1 == tree2) {
-                    common = tree1;
-                    flag = true;
-                }else {
-                    break;
-                }
-            }
-            if(flag) {
-                System.out.println("The common ancester is : "+common);
+            if(path1 == null || path2 == null) {
+                System.out.println("There is no common ancester");
             }
             else {
-                System.out.println("There is no common ancester");
+                while(!path1.isEmpty() && !path2.isEmpty()) {
+                    int tree1 = path1.pop();
+                    int tree2 = path2.pop();
+                    if(tree1 == tree2) {
+                        common = tree1;
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("The common ancester is : "+common);
             }
         }
     }
@@ -88,16 +78,5 @@ public class lowestCommonAncestor {
             }
         }
         return null;
-    }
-    
-    
-    
-    public void printTree(Node root) {
-        if(root != null)
-        {
-            System.out.print(root.getNodeVal()+" ");
-            printTree(root.getLeftNode());
-            printTree(root.getRightNode());
-        }
     }
 }
