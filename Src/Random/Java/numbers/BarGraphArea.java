be837401ddccc1be3bd97c40312303a62f94f35e
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-03-06 10:53:43 -0800

    Update BarGraphArea.java
    removed unused import - "import java.util.function.BiConsumer"

--- Src/Random/Java/numbers/BarGraphArea.java
+++ Src/Random/Java/numbers/BarGraphArea.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.function.BiConsumer;
 
 public class BarGraphArea {
     
