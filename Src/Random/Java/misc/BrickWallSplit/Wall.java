55388fd127d26b72bbea460440a4e1ba50c89cc8
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-03-07 01:22:58 -0800

    Create Wall.java

---
+++ Src/Random/Java/misc/BrickWallSplit/Wall.java
@@ -0,0 +1,18 @@
+package brickwallsplit;
+
+public class Wall {
+    
+    final Level[] level;
+    final int length;
+    final int height;
+    
+    public Wall(int height, int length) {
+        this.height = height;
+        this.length = length;
+        this.level = new Level[height];
+    }   
+    
+    public void setLevel(Level l, int index) {
+        this.level[index] = l;
+    }
+}
