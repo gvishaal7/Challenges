1c3c1b7edb0180ddf43072d537ea3e475147567e
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-03-07 01:23:37 -0800

    Create Level.java

---
+++ Src/Random/Java/misc/BrickWallSplit/Level.java
@@ -0,0 +1,15 @@
+package brickwallsplit;
+
+public class Level {
+    final int bricks;
+    final int[] length;
+    
+    public Level(int bricks) {
+        this.bricks = bricks;
+        this.length = new int[bricks];
+    }
+    
+    public void addLength(int l, int index) {
+        this.length[index] = l;
+    } 
+}
