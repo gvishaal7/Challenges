d937cd6b1128404fbda48214a0dc468f5b311b4a
Vishaal Gopalan <vishaalgopalan@gmail.com>
2018-10-16 16:40:10 -0500

    Commented the link

--- Src/Hackerrank/SQL/TypesofTriangle.sql
+++ Src/Hackerrank/SQL/TypesofTriangle.sql
@@ -1,4 +1,6 @@
-link: https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+/*
+ * link: https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+ */
 
 select  case when a+b <= c or c+a <= b or b+c <= a 
 	then 'Not A Triangle'
