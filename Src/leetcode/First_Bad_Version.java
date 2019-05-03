bd973f7bb2b94b83cced4a1030162d860bf9fbfa
Vishaal Gopalan <vishaalgopalan@gmail.com>
2019-05-03 00:34:08 -0500

    Create First_Bad_Version.java

---
+++ Src/leetcode/First_Bad_Version.java
@@ -0,0 +1,22 @@
+/*
+ * question: https://leetcode.com/problems/first-bad-version/
+ */
+
+public class First_Bad_Version extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 0;
+        int r = n;
+        int mid = l +((r-l)/2);
+        while(l != r) {
+            mid = l +((r-l)/2);
+            if(isBadVersion(mid)) {
+                r = mid;
+            }
+            else {
+                l = mid + 1;
+            }
+        } 
+        if(l != n && isBadVersion(l)) return l;
+        else return mid + 1;
+    }
+}
