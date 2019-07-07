1423a76a1bad8b7105013f317e478188e85e3774
gvishaal7 <vishaalgopalan@gmail.com>
2019-07-07 17:48:42 -0500

    added solution to minStack

---
+++ Src/leetcode/MinStack.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+/*
+ * question: https://leetcode.com/problems/min-stack/
+ */
+
+class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> myStack;
+    Stack<Integer> minStack;
+    public MinStack() {
+        myStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        myStack.add(x);
+        if(minStack.isEmpty()) {
+            minStack.add(x);
+        }
+        else {
+            int minVal = minStack.peek();
+            if(minVal >= x) {
+                minStack.add(x);
+            }
+        }
+    }
+
+    public void pop() {
+        int val = myStack.pop();
+        if(val == minStack.peek()) {
+            minStack.pop();
+        }
+
+    }
+
+    public int top() {
+        return myStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
