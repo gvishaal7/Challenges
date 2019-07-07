775e9f52ac66af0dd498f612683be58a48627cde
gvishaal7 <vishaalgopalan@gmail.com>
2019-07-07 16:41:17 -0500

    solved open the lock question from leetcode

---
+++ Src/leetcode/OpenTheLock.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+/*
+ * question link: https://leetcode.com/problems/open-the-lock/
+ */
+
+public class OpenTheLock {
+
+    public int openLock(String[] deadends, String target) {
+        String start = "0000";
+        int times = -1;
+        Set<String> alreadySeen = new HashSet<>();
+        Set<String> deadList = new HashSet<>(Arrays.asList(deadends));
+        Queue<Map> myQue = new LinkedList<>();
+        Map<String,Integer> tempMap = new HashMap<>();
+        tempMap.put(start,0);
+        if(!deadList.contains(start) && !alreadySeen.contains(start)) {
+            myQue.add(tempMap);
+            alreadySeen.add(start);
+        }
+        while(!myQue.isEmpty()) {
+            Map<String,Integer> first = myQue.poll();
+            String currStr = first.keySet().iterator().next();
+            int currVal = first.get(currStr);
+            if(currStr.equals(target)) {
+                times = currVal;
+                break;
+            }
+            currVal++;
+            for(int pos=0;pos<4;pos++)
+            {
+                String positiveTurn = rotate(currStr,pos,true);
+                String negativeTurn = rotate(currStr,pos,false);
+
+                tempMap = new HashMap<>();
+                tempMap.put(positiveTurn,currVal);
+                if(!deadList.contains(positiveTurn) && !alreadySeen.contains(positiveTurn)) {
+                    myQue.add(tempMap);
+                    alreadySeen.add(positiveTurn);
+                }
+
+                tempMap = new HashMap<>();
+                tempMap.put(negativeTurn, currVal);
+                if(!deadList.contains(negativeTurn) && !alreadySeen.contains(negativeTurn)) {
+                    myQue.add(tempMap);
+                    alreadySeen.add(negativeTurn);
+                }
+            }
+        }
+        return times;
+    }
+
+    private String rotate(String current, int position,boolean add) {
+        int val = Integer.parseInt(current.substring(position,position+1));
+        if(add) {
+            val++;
+            if(val > 9) {
+                val = 0;
+            }
+        }
+        else {
+            val--;
+            if(val < 0) {
+                val = 9;
+            }
+        }
+        String newString = current.substring(0,position)+String.valueOf(val)+current.substring(position+1);
+        return newString;
+    }
+
+}
