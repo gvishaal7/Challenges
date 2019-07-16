ba65c9d30088244c8ae5a7b71da205412cfe37f7
gvishaal7 <vishaalgopalan@gmail.com>
2019-07-16 16:12:41 -0500

    added trie module

---
+++ Src/Random/Java/Trie/BuildTrie.java
@@ -0,0 +1,24 @@
+public class BuildTrie {
+
+    TrieNode root;
+
+    public BuildTrie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        char[] wArr = word.toCharArray();
+        TrieNode current = root;
+
+        for(char w : wArr) {
+            TrieNode temp = current.children.get(w);
+            if(temp == null) {
+                temp = new TrieNode();
+                current.children.put(w,temp);
+            }
+            current = temp;
+        }
+
+        current.isEnd = true;
+    }
+}
---
+++ Src/Random/Java/Trie/SearchTrie.java
@@ -0,0 +1,14 @@
+public class SearchTrie {
+
+    public boolean search(TrieNode node, String word) {
+        char[] wArray = word.toCharArray();
+        for(char w : wArray) {
+            TrieNode temp = node.children.get(w);
+            if(temp == null) {
+                return false;
+            }
+            node = temp;
+        }
+        return node.isEnd;
+    }
+}
