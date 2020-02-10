768c1ad8dc240b64a0c825485c196f874b723e2a
gvishaal7 <vishaalgopalan@gmail.com>
2020-02-10 05:44:00 +0530

    added anangram

---
+++ Src/leetcode/MinStepsToMakeTwoStringsAnagram.java
@@ -0,0 +1,26 @@
+/**
+ * <a href="https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/"> question </a>
+ */
+
+public class MinStepsToMakeTwoStringsAnagram {
+
+	public int minSteps(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        
+        int[] sMap = new int[26];
+        for(char ss : tArray) {
+            sMap[ss-97]++;
+        }
+        for(char tt : sArray) {
+            sMap[tt-97]--;
+        }             
+        int count = 0;
+        for(int i : sMap) {
+            if(i > 0) {
+                count += i;
+            }
+        }
+        return count;            
+    }
+}
